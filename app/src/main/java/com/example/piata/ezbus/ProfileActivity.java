2e9a2bccfa2fac34cace51f7be6798e7aec270cf
Samuel Piatanesi <piatanesi.samuel@gmail.com>
2019-01-16 17:13:04 +0100

    Inserita pulsante indietro

--- app/src/main/java/com/example/piata/ezbus/ProfileActivity.java
+++ app/src/main/java/com/example/piata/ezbus/ProfileActivity.java
@@ -1,10 +1,12 @@
 package com.example.piata.ezbus;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,27 +15,19 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class ProfileActivity extends AppCompatActivity {
 
-
-
         private TextView email;
         private TextView username;
-        private TextView name;
-        private TextView surname;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_profile);
 
-            email = (TextView) findViewById(R.id.Email);
-            username = (TextView) findViewById(R.id.Username);
-            //name = (TextView) findViewById(R.id.Name);
-            //surname = (TextView) findViewById(R.id.Surname);
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            email = findViewById(R.id.Email);
+            username = findViewById(R.id.Username);
 
-            //get firebase auth instance
-            //auth = FirebaseAuth.getInstance();
-
-            //get current user
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             setDataToView(user);
         }
@@ -43,6 +37,12 @@ public class ProfileActivity extends AppCompatActivity {
             email.setText("Email: " + user.getEmail());
             username.setText("Username: " + user.getDisplayName());
             username.setText("User: " + user.getDisplayName());
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            finish();
+            return true;
         }
     }
 
