8f1b51d8c7931b0036d40c345eb8861c73dc86f5
Ruschio <luca.ruschioni@hotmail.it>
2019-01-17 11:56:43 +0100

    Add pocket tabs

--- app/src/main/java/com/example/piata/ezbus/PocketFragment.java
+++ app/src/main/java/com/example/piata/ezbus/PocketFragment.java
@@ -2,16 +2,25 @@ package com.example.piata.ezbus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PocketFragment extends Fragment implements View.OnClickListener {
 
     TextView text;
     View view;
+    private TabLayout tabLayout;
+    private ViewPager firstViewPager;
 
 
     public PocketFragment() {
@@ -22,8 +31,14 @@ public class PocketFragment extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_pocket, container, false);
-        text = view.findViewById(R.id.text_pocket);
-        text.setText("Ecco il tuo portafoglio");
+        //text = view.findViewById(R.id.text_pocket);
+        //text.setText("Ecco il tuo portafoglio");
+        firstViewPager = view.findViewById(R.id.viewpager);
+
+        tabLayout = view.findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(firstViewPager);
+
+        setupViewPager(firstViewPager);
         return view;
     }
 
@@ -39,6 +54,43 @@ public class PocketFragment extends Fragment implements View.OnClickListener {
             text.setText("Devi fare il login");
         } else {
             text.setText("Ecco il tuo portafoglio");
+        }
+    }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new BuyFragment(), "Tab 1");
+        adapter.addFragment(new BuyFragment(), "Tab 2");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
         }
     }
 
