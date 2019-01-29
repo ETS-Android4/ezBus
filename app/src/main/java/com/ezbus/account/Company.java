e2b8b16cb0a3f9e19a2b8219eb4bc34cca2634ed
Ludovico <ludovicoguercio88@gmail.com>
2019-01-29 10:57:04 +0100

    add class company

---
+++ app/src/main/java/com/ezbus/account/Company.java
@@ -0,0 +1,14 @@
+package com.ezbus.account;
+
+class Company {
+
+    private String company;
+    private String email;
+    private String iva;
+
+    public Company(String company, String email, String iva) {
+        this.company = company;
+        this.email = email;
+        this.iva = iva;
+    }
+}
