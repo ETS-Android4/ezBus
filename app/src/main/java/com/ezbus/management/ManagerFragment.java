f420e8fa19bd6c9f1b7391da72112403ae29ee2d
Ludovico <ludovicoguercio88@gmail.com>
2019-02-06 16:07:54 +0100

    button

--- app/src/main/java/com/ezbus/management/ManagerFragment.java
+++ app/src/main/java/com/ezbus/management/ManagerFragment.java
@@ -1,10 +1,13 @@
 package com.ezbus.management;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
 import com.ezbus.R;
 
 public class ManagerFragment extends Fragment {
@@ -17,8 +20,22 @@ public class ManagerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manager, container, false);
+
+        View rootView = inflater.inflate(R.layout.activity_route_manager, container, false);
+
+        Button button = (Button) rootView.findViewById(R.id.addRoute);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), route_manager.class);
+                startActivity(intent);
+            }
+        });
+
+        return rootView;
+
+            // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_manager, container, false);
     }
 
 }
