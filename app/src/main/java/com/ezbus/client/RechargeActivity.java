b5bd0d2e1c9cfcd51904fe3c6e5536390c3ac801
Ludovico <ludovicoguercio88@gmail.com>
2019-02-12 10:32:56 +0100

    recharge activity

---
+++ app/src/main/java/com/ezbus/client/RechargeActivity.java
@@ -0,0 +1,36 @@
+package com.ezbus.client;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.ezbus.R;
+import com.ezbus.main.SharedPref;
+
+public class RechargeActivity extends AppCompatActivity {
+
+    SharedPref sharedpref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        sharedpref = new SharedPref(this);
+        if (sharedpref.loadNightModeState())
+            setTheme(R.style.App_Dark);
+        else setTheme(R.style.App_Green);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recharge);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
+    }
+}
+
