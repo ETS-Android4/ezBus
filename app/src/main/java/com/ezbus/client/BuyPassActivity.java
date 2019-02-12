880ced93a10558cd4fd2aa5348a82b949e139490
Ludovico <ludovicoguercio88@gmail.com>
2019-02-12 10:32:17 +0100

    buypass activity

---
+++ app/src/main/java/com/ezbus/client/BuyPassActivity.java
@@ -0,0 +1,91 @@
+package com.ezbus.client;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.ezbus.R;
+import com.ezbus.authentication.LoginActivity;
+import com.ezbus.main.SharedPref;
+import com.ezbus.management.PassManagerActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuyPassActivity extends AppCompatActivity {
+
+    SharedPref sharedpref;
+    private ArrayAdapter mAdapter;
+    DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        sharedpref = new SharedPref(this);
+        if (sharedpref.loadNightModeState())
+            setTheme(R.style.App_Dark);
+        else setTheme(R.style.App_Green);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_pass);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        final ListView listPass = findViewById(R.id.buyable_pass);
+        List<String> initialList = new ArrayList<String>();
+        mAdapter = new ArrayAdapter(this, R.layout.row, R.id.textViewList, initialList);
+        listPass.setAdapter(mAdapter);
+        listPass.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(BuyPassActivity.this, listPass.getItemAtPosition(position).toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        aggiornaDati();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        finish();
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        aggiornaDati();
+    }
+
+    private void aggiornaDati() {
+        database.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mAdapter.clear();
+                for (DataSnapshot child : dataSnapshot.child("pass").getChildren()) {
+                    Pass p = child.getValue(Pass.class);
+                    mAdapter.add(p.getName());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
+
