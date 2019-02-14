5a6fbaf61c65cb78993c3b3821ea8b3d1d2ce23a
Froozex <froozextgh@gmail.com>
2019-02-14 11:03:36 +0100

    Fix Warnings part 2

--- app/src/main/java/com/ezbus/authentication/PrivacyActivity.java
+++ app/src/main/java/com/ezbus/authentication/PrivacyActivity.java
@@ -1,25 +1,39 @@
 package com.ezbus.authentication;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
-import com.ezbus.R;
-
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import com.ezbus.R;
+import com.ezbus.main.MainActivity;
+import com.ezbus.main.SharedPref;
+
 public class PrivacyActivity extends AppCompatActivity {
 
-    private String text;
-    private TextView privacy;
-    private TextView txtPrivacy;
 
+    public String text;
+    public TextView privacy;
+    public TextView txtPrivacy;
+    SharedPref sharedpref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        this.sharedpref = new SharedPref(this);
+        if (sharedpref.loadNightModeState())
+            setTheme(R.style.App_Dark);
+        else setTheme(R.style.App_Green);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy);
 
@@ -42,22 +56,27 @@ public class PrivacyActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             txtPrivacy.setText(text);
+
         }
+
 
     public void onClick(View view) {
         finish();
 
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         finish();
         return true;
     }
-
+    //Se viene premuto il pulsante Indietro di sistema
     @Override
     public void onBackPressed() {
         finish();
     }
 
 }
+
+
+
+
